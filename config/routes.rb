182954befe7274e19115b2e2623217cf288eab58
Eric Himmelreich <eric@rawsyntax.com>
2018-08-26 09:20:56 -0500

    Redirect root path to /articles listing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  root 'welcome#index'
+  root to: redirect('/articles')
+
   resources :articles do
     resources :comments
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
