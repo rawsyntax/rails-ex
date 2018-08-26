526bba413cbb727701c3e076f1a0fbc25965c926
Eric Himmelreich <eric@rawsyntax.com>
2018-08-26 15:02:02 -0500

    Require body field on an article
    Added unit tests for article validations

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "requires a title" do
+    article = Article.new
+    article.valid?
+
+    assert article.errors.messages[:title] == ["can't be blank", "is too short (minimum is 5 characters)"]
+  end
+
+  test "requires a body" do
+    article = Article.new
+    article.valid?
+
+    assert article.errors.messages[:body] == ["can't be blank", "is too short (minimum is 5 characters)"]
+  end
 end
