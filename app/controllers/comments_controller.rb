719611ec1e2d7f66ea4ffd92b69aae7945e0547b
Eric Himmelreich <eric@rawsyntax.com>
2018-08-26 15:06:21 -0500

    Add validations and tests to Comment model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -35,7 +35,7 @@ class CommentsController < ApplicationController
         format.html { redirect_to article_path(@article), notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
-        format.html { render :new }
+        format.html { render 'articles/show' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "requires a commenter" do
+    comment = Comment.new
+    comment.valid?
+
+    assert comment.errors.messages[:commenter] == ["can't be blank"]
+  end
+
+  test "requires a body" do
+    comment = Comment.new
+    comment.valid?
+
+    assert comment.errors.messages[:commenter] == ["can't be blank"]
+  end
 end
